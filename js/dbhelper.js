83ef75d28c2e66c3951b0b58ea50d1322db81bb1
Sherwino <sherwination@gmail.com>
2018-10-17 12:52:13 -0400

    feat: Now project should work with github pages

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,19 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const data = 'data/restaurants.json';
+    const github = `https://sherwino.github.io/wino-mws-restaurants/${data}`;
+    const port = 8000
+
+    const isLocalHost = () => {
+      if (window.location.hostname.includes('localhost')) {
+        return `http://localhost:${port}/${data}`;
+      }
+    };
+
+    const url = isLocalHost() || github;
+    
+    return url;
   }
 
   /**
