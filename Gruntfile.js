cdc6b0636b8d05c118d8af6938c694a6f03d9450
sherwino <sherwination@gmail.com>
2018-11-15 00:14:04 -0500

    feat: Grunt and Gulp script mods, and responsive images

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,45 @@
-'user strict';
+"user strict";
 
-module.exports = function (grunt) {
-    grunt.initConfig({
-        responsive_images: {
-          myTask: {
+module.exports = function(grunt) {
+  grunt.initConfig({
+    responsive_images: {
+        myTask: {
             options: {
               sizes: [
                 {
-                    name: "large",
-                    width: 1024,
-                    suffix: "-large",
-                    quality: 80
-                  }              
+                  name: "large",
+                  width: 1024,
+                //   suffix: "-large",
+                  quality: 80
+                },
+                {
+                  name: "medium",
+                  width: 800,
+                //   suffix: "-medium",
+                  quality: 80
+                },
+                {
+                  name: "small",
+                  width: 320,
+                //   suffix: "-small",
+                  quality: 60
+                }
+              ]
+            },
+            files: [
               {
-                name: "medium",
-                width: 800,
-                suffix: "-medium",
-                quality: 80
-              }
-              {
-                name: "small",
-                width: 320,
-                suffix: "-small",
-                quality: 60
+                expand: true,
+                cwd: "./src/img",
+                src: ["**.{jpg,gif,png}"],
+                dest: "./src/img/"
               }
             ]
-            },
-            files: [{
-              expand: true,
-              src: ['./src/img/**.{jpg,gif,png}'],
-              cwd: './test/',
-              dest: './dist/img/'
-            }]
-          }
-        },
-      })
-
-    grunt.loadNpmTasks('grunt-cwebp');
+        }
+    }
+  });
+    // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-responsive-images');
-    grunt.registerTask('default', ['responsive_images','cwebp']);
-  
-  };
-  
+
+    // Default task(s).
+    grunt.registerTask('default', ['responsive_images']);
+};
